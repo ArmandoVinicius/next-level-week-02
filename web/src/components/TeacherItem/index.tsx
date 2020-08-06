0a80414f5f08cec16526e5b21e164d4edd0906d4
ArmandoVinicius <armando.vinicius12@gmail.com>
2020-08-06 15:20:29 -0300

    Finalizado dia 3 da Next Level Week

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,38 +1,59 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
-    return (
-        <article className="teacher-item">
-            <header>
-                <img src="https://avatars1.githubusercontent.com/u/54650564?s=460&v=4" alt="Armando Vinicius"/>
-                <div>
-                    <strong>Armando Vinicius</strong>
-                    <span>Matemática</span>
-                </div>
-            </header>
+export interface Teacher {
+	avatar: string;
+	bio: string;
+	cost: number;
+	id: number;
+	name: string;
+	subject: string;
+	whatsapp: string;
+}
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-            </p>
-            
-            <footer>
-                <p>
-                    Preço/hora
-                    <strong>R$ 100,00</strong>
-                </p>
-                <button type="button">
-                    <img src={whatsappIcon} alt="Whatsapp"/>
-                    Entre em contato.
-                </button>
-            </footer>
-        </article>
-    );
+interface TeacherItemProps {
+	teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id,
+		})
+	}
+	
+	return (
+		<article className="teacher-item">
+			<header>
+				<img src={teacher.avatar} alt={teacher.name}/>
+				<div>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
+				</div>
+			</header>
+
+			<p>{teacher.bio}</p>
+				
+			<footer>
+				<p>
+					Preço/hora
+					<strong>R$ {teacher.cost}</strong>
+				</p>
+				<a 
+					target="_blank" 
+					onClick={createNewConnection} 
+					href={`https://wa.me/${teacher.whatsapp}`}
+				>
+					<img src={whatsappIcon} alt="Whatsapp"/>
+					Entre em contato.
+				</a>
+			</footer>
+		</article>
+	);
 }
 
 export default TeacherItem;
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -11,38 +12,48 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 function Landing() {
-    return (
-        <div id="page-landing">
-            <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="Logo Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
-                </div>
+	const [ totalConnections, setTotalConnections ] = useState(0);
 
-                <img 
-                    src={landingImg} 
-                    alt="Plataforma de estudos" 
-                    className="hero-image"
-                />
+	useEffect(() => {
+		api.get('connections').then(response => {
+			const { total } = response.data;
 
-                <div className="buttons-container">
-                    <Link to="study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </Link>
+			setTotalConnections(total);
+		})
+	}, []);
 
-                    <Link to="give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Dar aula"/>
-                        Dar aulas
-                    </Link>
-                </div>
+	return (
+		<div id="page-landing">
+			<div id="page-landing-content" className="container">
+				<div className="logo-container">
+					<img src={logoImg} alt="Logo Proffy"/>
+					<h2>Sua plataforma de estudos online.</h2>
+				</div>
 
-                <span className="total-conections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-                </span>
-            </div>
-        </div>
-    );
+				<img 
+					src={landingImg} 
+					alt="Plataforma de estudos" 
+					className="hero-image"
+				/>
+
+				<div className="buttons-container">
+					<Link to="study" className="study">
+						<img src={studyIcon} alt="Estudar"/>
+						Estudar
+					</Link>
+
+					<Link to="give-classes" className="give-classes">
+						<img src={giveClassesIcon} alt="Dar aula"/>
+						Dar aulas
+					</Link>
+				</div>
+
+				<span className="total-conections">
+					Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default Landing;
