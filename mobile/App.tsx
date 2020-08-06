cb3ed873fe0a4d346436953de2cc5626d11ca5c5
ArmandoVinicius <armando.vinicius12@gmail.com>
2020-08-06 15:21:42 -0300

    Commit inicial do dia 4 da Next Level Week

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hello NLW</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+});
