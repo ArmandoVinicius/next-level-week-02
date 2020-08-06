189f296a5d4993f809959eee11214cfc523de279
ArmandoVinicius <armando.vinicius12@gmail.com>
2020-08-06 17:41:10 -0300

    Finalizado dia 4 da Next Level Week

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Hello NLW</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  texto: {
-    fontSize: 32,
-    color: '#fff',
-    fontWeight: 'bold'
+import AppStack from './src/routes/AppStack';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+  
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
   }
-});
+}
